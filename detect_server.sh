46c10c935e1575fc57084942afb9bb88ec09d77d
blackbbc <505968815@qq.com>
2015-04-09 00:29:20 +0800

    change to ash

--- detect_server.sh
+++ detect_server.sh
@@ -1,16 +1,22 @@
 #/bin/sh
 servers=" \
+sg04.shadowsocks.com.cn \
 tw01.shadowsocks.com.cn \
 jp02.shadowsocks.com.cn \
 jp03.shadowsocks.com.cn \
-jp03.shadowsocks.com.cn \
+jp04.shadowsocks.com.cn \
+jp05.shadowsocks.com.cn \
+hk01.shadowsocks.com.cn \
+hk03.shadowsocks.com.cn \
+hk04.shadowsocks.com.cn \
 "
 
 minn=1000000
 for server in $servers
 do
     avgPing=`ping -c 4 ${server} | tail -1| awk '{print $4}' | cut -d '/' -f 2`
-    if [ $(echo "$avgPing < $minn" | bc) -ne 0 ]
+    avgPing=${avgPing%.*}
+    if [ $avgPing -lt $minn ]
     then
         minn=$avgPing
         bestServer=$server
