ebc98935c3b89dc77f841ad988739dd952a93baa
blackbbc <505968815@qq.com>
2015-05-21 14:07:48 +0800

    optimize code

--- test.sh
+++ test.sh
@@ -1,16 +1,20 @@
 #/bin/sh
 servers=" \
-sg04.shadowsocks.com.cn \
-jp03.shadowsocks.com.cn \
-hk03.shadowsocks.com.cn \
-hk04.shadowsocks.com.cn \
+vip-sg04-1.ssv4.net \
+vip-tw01-1.ssv4.net \
+vip-jp02-1.ssv4.net \
+vip-jp04-1.ssv4.net \
+vip-jp05-1.ssv4.net \
+vip-hk01-1.ssv4.net \
+vip-hk03-1.ssv4.net \
+vip-hk04-1.ssv4.net \
 "
 
 minn=1000000
 for server in $servers
 do
-    ping -c 1 $server
-    if test $? -eq 0
+    ping -c 1 -W 1 $server > /dev/null
+    if [ "$?" = "0" ]
     then
         avgPing=`ping -c 4 ${server} | tail -1| awk '{print $4}' | cut -d '/' -f 2`
         avgPing=${avgPing%.*}
